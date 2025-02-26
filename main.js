cdd40be33e830a2c34bc1b2bbcc9ac35d34af413
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-25 22:36:02 -0300

    Refactored main to use interactions instead of messages

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Partials, Events, Collection, EmbedBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Events, Collection, MessageFlags } = require('discord.js');
 const dotenv = require('dotenv').config()
 const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds], partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
 
@@ -9,15 +9,13 @@ const path = require('path');
 
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands/')
+    .filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    if (command.name) {
-        client.commands.set(command.name, command);
-    } else {
-        console.warn(`⚠️ Comando ${file} não tem uma propriedade 'name' válida.`);
-    }
+
+    client.commands.set(command.name || command.data.name, command);
 }
 
 
@@ -26,35 +24,18 @@ client.once('ready', () => {
     console.log('Tars is online');
 });
 
-client.on(Events.MessageCreate, (message) =>{    
-    if(!message.content.startsWith(prefix) || message.author.bot){
-        console.log('not for me baby');
-        return;
-    };
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    // switch(command){
-    //     case 'ping':
-    //         client.commands.get('ping').execute(message, args);
-    //         break
-    // }
-    const cmd = client.commands.get(command);
-
-if (!cmd) {
-    return message.channel.send('Esse comando eu não conheço, sô');
-}
-
-try {
-    cmd.execute(message, args, client);
-} catch (err) {
-    console.error(`Erro ao executar o comando ${command}:`, err);
-    message.channel.send('Ocorreu um erro ao executar esse comando.');
-}
-
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error)
+        await interaction.editReply('Ocorreu um erro ao executar o comando!');
+    }
 });
-
 
 //This has to be the last line of the file
 client.login(process.env.CLIENT_TOKEN);
