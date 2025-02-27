68029fde6c422478227d9fdbec6ed3317e1f3bde
Gustavo Isoo Binder <gustan.binder@gmail.com>
2025-02-27 10:47:49 -0300

    bday register changes

--- commands/register_bd.js
+++ commands/register_bd.js
@@ -11,18 +11,29 @@ module.exports = {
                 .setRequired(true)
         )
         .addStringOption(option =>
-            option.setName('data')
-                .setDescription('Formato: mm-dd')
+            option.setName('dia')
+                .setDescription('Dia do mês do seu aniversário')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('mês')
+                .setDescription('Mês do seu aniversário')
                 .setRequired(true)
         ),
     async execute(interaction) {
         await interaction.deferReply({ flags: MessageFlags.Crossposted })
 
         try {
-            const data = interaction.options.getString('data')
+            var dia = interaction.options.getString('dia')
+            var mes = interaction.options.getString('mês')
             const nome = interaction.options.getString('nome')
 
-            await pool.query("INSERT INTO birthday VALUES($1, $2);", [data, nome]);
+            dia = dia.length < 2 ? ('0' + dia) : (dia)
+            mes = mes.length < 2 ? ('0' + mes) : (mes)
+
+            const dateToInsert = mes + '-' + dia
+
+            await pool.query("INSERT INTO birthday VALUES($1, $2);", [dateToInsert, nome]);
 
             await interaction.editReply('Zezé agora sabe o níver de ' + nome + '!')
         } catch (err) {
